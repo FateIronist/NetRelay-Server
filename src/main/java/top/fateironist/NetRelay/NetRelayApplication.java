ff63763ed9bcf519c36dd26a0307436038e45a4a
fate <3513625074@qq.com>
2026-01-17 11:51:18 +0800

    init

---
+++ src/main/java/top/fateironist/NetRelay/NetRelayApplication.java
@@ -0,0 +1,13 @@
+package top.fateironist.NetRelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetRelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetRelayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/fateironist/NetRelay/NetRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package top.fateironist.NetRelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetRelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
